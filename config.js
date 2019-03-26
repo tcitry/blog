1781348e2b2e5300f2cb25e022810f6cb356f9d7
tcitry <tcitry@gmail.com>
2019-03-26 18:15:08 +0800

    init issues-blog

---
+++ config.js
@@ -0,0 +1,8 @@
+var _config = {
+    blog_name       : '殷东亮的博客',
+    owner           : 'tcitry',
+    repo            : 'tcitry.github.io',
+    access_token : '6d52100826acbb1a967d'+'6c36047c072f4a20d75b',
+    per_page        : '15'
+}
+
