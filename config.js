9da58ef07f4b13be30a543fd2c96660ced43b9bb
tcitry <tcitry@gmail.com>
2019-07-06 09:52:10 +0800

    update readme

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 var _config = {
-    blog_name       : '殷东亮的博客',
-    owner           : 'tcitry',
-    repo            : 'tcitry.github.io',
-    access_token : '6d52100826acbb1a967d'+'6c36047c072f4a20d75b',
-    per_page        : '15'
+    blog_name       : '用于演示的博客',         // 博客名称
+    owner           : 'tcitry',           // github 用户名
+    repo            : 'tcitry.github.io',// github 中对应项目名
+    access_token : '6d52100826acbb1a967d'+'6c36047c072f4a20d75b',       // 请求量大时需要在 github 后台单独设置一个读取公开库的 token, 注意将token 拆成两个字符串，否则会被系统自动删除掉
+    per_page        : '15'                    // 默认一页显示几篇文章
 }
-
